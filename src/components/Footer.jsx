f5595438a47a4f810f7aecca041a96862d471306
JUNHYUK JUNG <kuyhnuj@outlook.com>
2024-02-11 01:40:11 +0900

    Layout & Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,26 +1,34 @@
 import React from "react";
+import { BsGithub } from "react-icons/bs";
 
 const Footer = () => {
   return (
     <footer className="text-gray-600 body-font">
-      <div className="bg-gray-100">
+      <div className="md:border-t md:border-gray-400 mx-16">
         <div className="containe r px-5 py-6 mx-auto flex items-center sm:flex-row flex-col mx-20">
-          <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-            {/* <svg>svg 이미지 넣는 곳</svg> */}
-            <span className="ml-3 text-xl">정준혁 포트폴리오</span>
-          </a>
-          <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">
-            © {new Date().getFullYear()} JUNHYUKJUNG PORTFOLIO —
+          <div className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+            LOGO
+            <span className="ml-3 text-xl dao-market">DAO MARKET</span>
+          </div>
+          <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4 flex">
+            © {new Date().getFullYear()} DAO MARKET BCS4 PROJECT TEAM 4 —
             <a
-              href="https://github.com/JUNHYUKJUNG"
+              href="https://github.com/daomarket4/daomarket"
               rel="noopener noreferrer"
-              className="text-gray-600 ml-1"
+              className="text-blue-700 ml-1"
               target="_blank"
             >
-              @junhyukjung
+              @ BCS4 - PROJECT TEAM 4 GITHUB
             </a>
           </p>
-          <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+          <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center mr-8 sm:justify-start">
+            <a
+              href="https://github.com/daomarket4/daomarket"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <BsGithub className="text-xl mr-3" />
+            </a>
             <a className="text-gray-500">
               <svg
                 fill="currentColor"
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,9 +1,13 @@
+import Layout from "../components/Layout";
+
 const Project = () => {
   return (
     <div className="bg-darkMode">
-      <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
-        참여 페이지
-      </section>
+      <Layout>
+        <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
+          프로젝트
+        </section>
+      </Layout>
     </div>
   );
 };
