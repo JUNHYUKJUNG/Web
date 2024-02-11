9038ecc86328712307073fd05d627e617aa3bbe7
JUNHYUK JUNG <kuyhnuj@outlook.com>
2024-02-12 01:20:09 +0900

    DarkMode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
       <header className="text-gray-600 body-font">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <Link to="/">
-            <div className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+            <div className="flex title-font font-medium items-center text-gray-900 dark:text-white mb-4 md:mb-0">
               이미지
               <span className="ml-3 text-xl">정준혁 포트폴리오</span>
             </div>
